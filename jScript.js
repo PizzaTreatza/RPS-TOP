2fd59de4cde301a4398e4d290b6c513f1ce6876a
PizzaTreatza-Away <mitchelld58@gmail.com>
2023-03-16 11:59:52 -0800

    making changes

--- jScript.js
+++ jScript.js
@@ -1,8 +1,14 @@
-let cChoice;
+            let cChoice;
             let pChoice;
             let roundResult;
             let playerScore = 0;
             let computerScore = 0;
+
+            const cScore = document.querySelector('#compScore');
+            const pScore = document.querySelector('#playerScore');
+
+            cScore.textContent = computerScore;
+            pScore.textContent = playerScore;
 
             function getComputerChoice() {
                 cChoice = Math.floor((Math.random()*3)+1);
@@ -26,9 +32,10 @@ let cChoice;
                 else if (playerSelection == "scissors" && computerSelection == "paper") {roundResult = "Player Win";}
                 else {roundResult = "Computer Win";}
                 return roundResult;
+                alert(roundResult);
             }
             function game() {
-                
+
                     let playerSelection = getPlayerChoice();
                     let computerSelection = getComputerChoice();
                     
@@ -40,3 +47,48 @@ let cChoice;
                 else if (playerScore < computerScore) {alert("You lose");}
                 else if (playerScore == computerScore) {alert("Tie game");}
             }
+
+            const rButton = document.querySelector('.rBtn');
+            rButton.addEventListener('click', function (e) {
+
+                let computerSelection = getComputerChoice();
+                let playerSelection = "rock"
+
+                console.log(playRound(playerSelection, computerSelection));
+                    if (roundResult == "Player Win") {playerScore++; pScore.textContent = playerScore;}
+                    else if (roundResult == "Computer Win") {computerScore++; cScore.textContent = computerScore;}
+                
+                if (playerScore > computerScore) {console.log("You won the game!");}
+                else if (playerScore < computerScore) {console.log("You lose");}
+                else if (playerScore == computerScore) {console.log("Tie game");};
+                 });
+
+            const pButton = document.querySelector('.pBtn');
+            pButton.addEventListener('click', function (e) {
+
+                let computerSelection = getComputerChoice();
+                let playerSelection = "paper"
+
+                console.log(playRound(playerSelection, computerSelection));
+                    if (roundResult == "Player Win") {playerScore++; pScore.textContent = playerScore;}
+                    else if (roundResult == "Computer Win") {computerScore++; cScore.textContent = computerScore;}
+                
+                if (playerScore > computerScore) {console.log("You won the game!");}
+                else if (playerScore < computerScore) {console.log("You lose");}
+                else if (playerScore == computerScore) {console.log("Tie game");};
+                 });
+
+            const sButton = document.querySelector('.sBtn');
+            sButton.addEventListener('click', function (e) {
+
+                let computerSelection = getComputerChoice();
+                let playerSelection = "scissors"
+
+                console.log(playRound(playerSelection, computerSelection));
+                    if (roundResult == "Player Win") {playerScore++; pScore.textContent = playerScore;}
+                    else if (roundResult == "Computer Win") {computerScore++; cScore.textContent = computerScore;}
+                
+                if (playerScore > computerScore) {console.log("You won the game!");}
+                else if (playerScore < computerScore) {console.log("You lose");}
+                else if (playerScore == computerScore) {console.log("Tie game");};
+                 });
