15b2354c33e8ab979dbf22c7b738956b1cf95e22
PizzaTreatza-Away <mitchelld58@gmail.com>
2024-02-01 08:49:34 -0900

    refinements

---
+++ jScript2.js
@@ -0,0 +1,112 @@
+            let cChoice;
+            let pChoice;
+            let roundResult;
+            let playerScore = 0;
+            let computerScore = 0;
+
+            const cScore = document.querySelector('#compScore');
+            const pScore = document.querySelector('#playerScore');
+            const scoreOutput = document.querySelector('#scoreOutput');
+
+            cScore.textContent = computerScore;
+            pScore.textContent = playerScore;
+
+            function getComputerChoice() {
+                cChoice = Math.floor((Math.random()*3)+1);
+                switch (cChoice) {
+                    case 1:
+                        return ("rock")
+                        break;
+                    case 2:
+                        return ("paper")
+                        break;
+                    case 3:
+                        return ("scissors")
+                        break;
+                }
+  //              if (cChoice == 1) {cChoice = "rock";}
+   //             else if (cChoice == 2) {cChoice = "paper";}
+   //             else {cChoice = "scissors";}
+    //            return cChoice;
+            }
+            function getPlayerChoice() {
+                pChoice = prompt("Rock, Paper, Scissors?");
+                pChoice = pChoice.toLowerCase();
+                if (pChoice != "rock" && pChoice != "paper" && pChoice != "scissors") { 
+                    alert("Improper input");
+                    getPlayerChoice();}
+                return pChoice;
+            }
+            function playRound(playerSelection, computerSelection) {
+                if (playerSelection == computerSelection) {roundResult = "Tie";}
+                else if (playerSelection == "rock" && computerSelection == "scissors") {roundResult = "Player Win";}
+                else if (playerSelection == "paper" && computerSelection == "rock") {roundResult = "Player Win";}
+                else if (playerSelection == "scissors" && computerSelection == "paper") {roundResult = "Player Win";}
+                else {roundResult = "Computer Win";}
+                scoreOutput.textContent = roundResult;
+                return roundResult;
+            }
+            function whoWon(playerScore, computerScore) {
+                if (playerScore > computerScore) {console.log("You won the game!");}
+                    else if (playerScore < computerScore) {console.log("You lose");}
+                    else if (playerScore == computerScore) {console.log("Tie game");};
+            }
+
+
+            // START GAME
+            function game() {
+
+                    let playerSelection = getPlayerChoice();
+                    let computerSelection = getComputerChoice();
+                    
+                    console.log(playRound(playerSelection, computerSelection));
+                    if (roundResult == "Player Win") {playerScore++;}
+                    else if (roundResult == "Computer Win") {computerScore++;}
+                
+                if (playerScore > computerScore) {alert("You won the game!");}
+                else if (playerScore < computerScore) {alert("You lose");}
+                else if (playerScore == computerScore) {alert("Tie game");}
+            }
+
+            const rButton = document.querySelector('.rBtn');
+            rButton.addEventListener('click', function (e) {
+
+                let computerSelection = getComputerChoice();
+                let playerSelection = "rock"
+
+                console.log(playRound(playerSelection, computerSelection));
+                    if (roundResult == "Player Win") {playerScore++; pScore.textContent = playerScore;}
+                    else if (roundResult == "Computer Win") {computerScore++; cScore.textContent = computerScore;}
+                
+                if (playerScore >= 5 || computerScore >= 5) { whoWon (playerScore, computerScore);} 
+                 });
+
+            const pButton = document.querySelector('.pBtn');
+            pButton.addEventListener('click', function (e) {
+
+                let computerSelection = getComputerChoice();
+                let playerSelection = "paper"
+
+                console.log(playRound(playerSelection, computerSelection));
+                    if (roundResult == "Player Win") {playerScore++; pScore.textContent = playerScore;}
+                    else if (roundResult == "Computer Win") {computerScore++; cScore.textContent = computerScore;}
+                
+                if (playerScore > computerScore) {console.log("You won the game!");}
+                else if (playerScore < computerScore) {console.log("You lose");}
+                else if (playerScore == computerScore) {console.log("Tie game");};
+                 });
+
+            const sButton = document.querySelector('.sBtn');
+            sButton.addEventListener('click', function (e) {
+
+                let computerSelection = getComputerChoice();
+                let playerSelection = "scissors"
+
+                console.log(playRound(playerSelection, computerSelection));
+                    if (roundResult == "Player Win") {playerScore++; pScore.textContent = playerScore;}
+                    else if (roundResult == "Computer Win") {computerScore++; cScore.textContent = computerScore;}
+                
+                if (playerScore > computerScore) {console.log("You won the game!");}
+                else if (playerScore < computerScore) {console.log("You lose");}
+                else if (playerScore == computerScore) {console.log("Tie game");};
+                 });
